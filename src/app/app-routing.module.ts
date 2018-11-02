38bc3a8e0bff711c954a97c097e873be9b215a41
fletchertyler914 <tyler.fletcher@navihealth.com>
2018-11-01 23:10:07 -0500

    Ionic CLI Testing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: './list/list.module#ListPageModule'
+  },
+  {
+    path: 'user-details',
+    loadChildren: './user-details/user-details.module#UserDetailsPageModule'
   }
 ];
 
